941df0e231a6b22914ebb1078c6d915aeb1c563e
Sudarshana V Sharma <svsccodes@gmail.com>
2026-06-10 17:51:22 +0530

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -5,6 +5,8 @@ def main():
     while True:
         sys.stdout.write("$ ")
         user_input = input()
+        if user_input == "exit":
+            break
         print(f"{user_input}: command not found")
 
 if __name__ == "__main__":
