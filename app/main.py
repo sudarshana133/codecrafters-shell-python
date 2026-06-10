4c6cd59966389fe48bc1526f1385f321b01b474d
Sudarshana V Sharma <svsccodes@gmail.com>
2026-06-10 18:09:45 +0530

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -9,15 +9,30 @@ def echo(user_input):
     return output
 
 
+def type_cmd(user_input) -> str:
+    lst = user_input.split(" ")
+    lst.pop(0)
+    cmd = lst[0]
+    if cmd in ("echo", "type", "exit"):
+        return f"{cmd} is a shell builtin"
+    return f"{cmd}: not found"
+
+
 def main():
     while True:
         sys.stdout.write("$ ")
         user_input = input()
         if user_input == "exit":
             break
+        cmd = user_input.split(" ")[0]
 
-        if "echo" in user_input:
+        if cmd == "echo":
             output = echo(user_input)
+            print(output)
+            continue
+
+        if cmd == "type":
+            output = (type_cmd(user_input))
             print(output)
             continue
 
