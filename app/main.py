09bfce1e4ce229e9809c8f7a105fa29668c12272
Sudarshana V Sharma <svsccodes@gmail.com>
2026-06-10 17:58:28 +0530

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,13 +1,28 @@
 import sys
 
 
+def echo(user_input):
+    lst = user_input.split(" ")
+    output = ""
+    for i in range(1, len(lst)):
+        output += lst[i] + " "
+    return output
+
+
 def main():
     while True:
         sys.stdout.write("$ ")
         user_input = input()
         if user_input == "exit":
             break
+
+        if "echo" in user_input:
+            output = echo(user_input)
+            print(output)
+            continue
+
         print(f"{user_input}: command not found")
+
 
 if __name__ == "__main__":
     main()
