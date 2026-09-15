ff034e060e88bdb2832e6bfea3d314714929f68f
Sudarshana V Sharma <svsccodes@gmail.com>
2026-09-15 21:00:52 +0530

    codecrafters submit [skip ci]

--- app/utils/auto_completer.py
+++ app/utils/auto_completer.py
@@ -1,15 +1,30 @@
+import os
+
+
 class AutoCompleter:
     def __init__(self) -> None:
-        self.commands = ["echo", "exit"]
+        self.builtins = ["echo", "type", "exit", "pwd", "cd"]
+
+    def get_executables(self):
+        directories = os.environ.get("PATH", "").split(":")
+        executables = []
+
+        for directory in directories:
+            if os.path.exists(directory):
+                dir_list = os.listdir(directory)
+                executables += dir_list
+
+        return executables
 
     def completer(self, text, state):
         """
         'text' is current word being completed.
         'state' is used by readline to cal this completer
         """
-        matches = []
-        if state == 0:
-            matches = [c + " " for c in self.commands if c.startswith(text)]
+
+        executables = self.get_executables()
+        all_options = set(self.builtins + executables)
+        matches = [c + " " for c in all_options if c.startswith(text)]
 
         try:
             return matches[state]
