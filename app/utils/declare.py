fa1cdac0058caaaac87b7583a9576a5a9e9919cc
Sudarshana V Sharma <svsccodes@gmail.com>
2026-09-18 17:37:57 +0530

    codecrafters submit [skip ci]

--- app/utils/declare.py
+++ app/utils/declare.py
@@ -3,14 +3,30 @@ from app.utils.helpers import helpers
 
 class Declare:
     def __init__(self):
-        pass
+        """
+        Stores the variable and it's value
+        e.g: {"var": "value"}
+        """
+        self.variables = {}
 
-    def run_declare(self, args: list[str]):
+    def run_declare(self, user_input: str, args: list[str]):
         # index of -p
         pIndex = helpers.get_index(args, "-p")
         if pIndex != -1:
             var = args[pIndex + 1]
-            print(f"declare: {var}: not found")
+            if var in self.variables:
+                val = self.variables[var]
+                print(f'declare -- {var}="{val}"')
+            else:
+                print(f"declare: {var}: not found")
+            return
+
+        # get index of =
+        for arg in args:
+            if "=" in arg:
+                var = arg.split("=")[0]
+                value = arg.split("=")[1]
+                self.variables[var] = value
 
 
 declare = Declare()
